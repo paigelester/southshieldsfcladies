b95d80376dbf08d3e54e406f84ce11dea9eae9ed
Paige Lester <paigelester7@gmail.com>
2019-07-30 22:47:37 +0100

    Added about page.

--- src/pages/main/about/index.tsx
+++ src/pages/main/about/index.tsx
@@ -4,7 +4,19 @@ export default class About extends React.Component<{}> {
     public render(): JSX.Element {
         return (
             <div>
-                About
+                <div>
+                    <h1>About us</h1>
+                </div>
+                <div>
+                    <p>South Shields FC Ladies was founded in June 2013 to give young women over 16 in the town and surrounding areas a chance to develop their footballing skills and be part of a new and exciting team.</p>
+                    <p>Founder and Manager Stephen Lester realised there were a large number of girls coming out of junior football with limited opportunity to progress into a senior team and continue their football education. He was surprised that a town the size and with the footballing history of South Shields didn’t have a senior ladies team.</p>
+                    <p>Our players are currently aged 16 to 35 and come from various backgrounds and locations across the region including South Shields, Hebburn, Sunderland, Durham and Newcastle.</p>
+                    <p>We are fully affiliated with South Shields Football Club who, with the addition of the ladies team, run teams from tots to seniors with FA Charter standard status. Although informal and fun, our club does believe in the core basic skills for running a rewarding and safe environment for players to thrive with dedicated FA qualified coaches and club welfare officer attending both training sessions and games.</p>
+                    <p>Although just completing our fifth season, we have enjoyed three promotions and in year three expanded to also run a Reserve Ladies team which gives even more opportunity for women in our area to get involved and develop. We are always on the lookout for new players to grow and improve the existing squads and also increase the number of our teams. A third team would be great!!</p>
+                    <p>As a self-funding and non-profit organisation we are actively looking for sponsors to get involved and help support the club. Running the teams is a very expensive hobby, so any help with this would be greatly appreciated. It’s a fantastic way for businesses to help and get involved in the community. We have our Facebook, Twitter, Instagram and Website platforms available for advertising. The club social media sites are read by thousands of local people and we enjoy the close relations we build with our sponsors.</p>
+                    <p>The club is focused on providing the best possible facilities and equipment available to our players at the lowest achievable cost. All abilities are catered for and we encourage our ladies to develop as respectful and positive young adults both on and off the pitch.</p>
+                    <p>If you want to join or get involved with the new home of Ladies football in the north east whether as a player, coach or sponsor please get in touch via our contact us page. We’d love to hear from you!!</p>
+                </div>
             </div>
         );
     }
